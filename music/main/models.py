8edd2aefd6dac3fe6607090607885ec5e88ef6bd
Daniil <daniil@gmail.com>
2026-03-26 12:37:07 +0700

    добавлена модель исполнителя

--- music/main/models.py
+++ music/main/models.py
@@ -9,11 +9,21 @@ class Genre(models.Model):
     # функция для преобразования в строку
     def __str__(self):
         return self.name_ru
+
+# Исполнитель (группа или человек)
+class Artist(models.Model):
+    name = models.CharField(max_length=500, unique=True)
+    image = models.ImageField(upload_to='artists/', null=True)
+
+    def __str__(self):
+        return self.name
     
 class Track(models.Model):
     title = models.CharField(max_length=500, unique=True)
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
+    artist = models.ForeignKey(Artist, on_delete=models.RESTRICT, null=True)
+    audio_file = models.FileField(upload_to='music/tracks/', blank=True, null=True)
 
     # функция для преобразования в строку
     def __str__(self):
